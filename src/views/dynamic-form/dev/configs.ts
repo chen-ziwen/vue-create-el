1fc3a672f5232bc07fd226ead5b6052ca3a7126a
Chiko <2452559902@qq.com>
2024-01-18 21:44:06 +0800

    fixed: 简单修改

--- src/views/dynamic-form/dev/configs.ts
+++ src/views/dynamic-form/dev/configs.ts
@@ -1,8 +1,5 @@
-// 用于渲染form表单 表单渲染时并不提供初始值 由后续每一项赋值
-// 表单的渲染和表单每一项的值分开写
-// 
+
 export default {
-    // 这里面可以分块渲染
     title: {
         name: "名称",
         form: {
@@ -52,40 +49,4 @@ export default {
         },
     }
 
-}
-
-// 表单每一项的真实数据
-const item = {
-    title: "自动后退",
-    condition: "gift",
-    gift: "none",
-    chat: "",
-    // xxx 还有很多
-}
-
-// 实际的数据应该是 (mock data)
-// 因为同一时间最多选中一项 所以可以这么写
-// 不然就是每一项item都必须有一个单独的id 同一组内的id不能重复
-const totalDatas = {
-    '永杰无间': {
-        id: "1",
-        datas: {
-            idx: 0, // 选中datas中的第一项
-            data: [item, item, item], // 对每一项进行增删改查
-        },
-    },
-    "王者荣耀": {
-        id: "2",
-        datas: {
-            idx: 1, // 选中datas中的第二项
-            data: [item, item, item]
-        },
-    },
-    "和平精英": {
-        id: "3",
-        datas: {
-            idx: -1, // 未选中
-            data: [item, item, item]
-        },
-    },
 }
--- src/views/dynamic-form/dynamic-form-item/form-item.vue
+++ src/views/dynamic-form/dynamic-form-item/form-item.vue
@@ -7,21 +7,19 @@
                     <template #content>
                         <div v-html="useFiltersHtmlstrip(config.tips, {}, true)"></div>
                     </template>
-                    <!-- 这边放提示图标 i-->
                     <i class="iconfont icon-help"></i>
                 </el-tooltip>
             </div>
         </template>
         <el-input size="small" v-if="form.type == 'text'" v-model="$bvalue" @blur="blur" :maxlength="form.maxlength"
-            :minlength="form.minlength" :placeholder="form.placeholder" @change="changeHook">
+            :minlength="form.minlength" :placeholder="form.placeholder">
             <template #prepend v-if="form.prepend">{{ form.prepend }}</template>
             <template #append v-if="form.append">{{ form.append }}</template>
         </el-input>
         <el-input size="small" v-else-if="form.type == 'textarea'" type="textarea"
-            :autosize="{ minRows: form.min || 2, maxRows: form.max || 6 }" v-model="$bvalue" @blur="blur"
-            @change="changeHook"></el-input>
+            :autosize="{ minRows: form.min || 2, maxRows: form.max || 6 }" v-model="$bvalue" @blur="blur"></el-input>
         <el-select size="small" v-else-if="form.type == 'select'" v-model="$value" :filterable="form.filterable || false"
-            :loading="loading" @change="changeHook">
+            :loading="loading">
             <el-option v-for="item in options" :label="item.label" :value="item.value">
                 <span class="f-l">{{ item.label }}</span>
                 <span v-if="item.icon" class="option-image flex-align-center f-r">
@@ -29,32 +27,33 @@
                 </span>
             </el-option>
         </el-select>
-        <el-switch size="small" v-else-if="form.type == 'switch'" v-model="$value" @change="changeHook"></el-switch>
+        <el-switch size="small" v-else-if="form.type == 'switch'" v-model="$value"></el-switch>
         <el-input-number size="small" v-else-if="form.type == 'number'" :min="form.min" :max="form.max"
-            :step="form.step || 1" v-model="$value" @change="changeHook"></el-input-number>
+            :step="form.step || 1" v-model="$value"></el-input-number>
         <el-slider size="small" v-else-if="form.type == 'slider'" :min="form.min || 0" :max="form.max || 100"
-            :step="form.step || 1" v-model="$value" :show-input="form.input || false" @change="changeHook"></el-slider>
-        <el-checkbox-group size="small" v-else-if="form.type == 'checkbox'" v-model="$value" :min="form.min" :max="form.max"
-            @change="changeHook">
+            :step="form.step || 1" v-model="$value" :show-input="form.input || false"></el-slider>
+        <el-checkbox-group size="small" v-else-if="form.type == 'checkbox'" v-model="$value" :min="form.min"
+            :max="form.max">
             <el-checkbox v-for="option in options" :key="option.value" :label="option.value">{{ option.label
             }}</el-checkbox>
         </el-checkbox-group>
-        <el-radio-group size="small" v-else-if="form.type == 'radio'" v-model="$value" @change="changeHook">
+        <el-radio-group size="small" v-else-if="form.type == 'radio'" v-model="$value">
             <el-radio v-for="option in options" :key="option.value" :label="option.value">{{ option.label }}</el-radio>
         </el-radio-group>
-        <el-radio-group size="small" v-else-if="form.type == 'radiobtn'" v-model="$value" @change="changeHook">
+        <el-radio-group size="small" v-else-if="form.type == 'radiobtn'" v-model="$value">
             <el-radio-button v-for="option in options" :key="option.value" :label="option.value">{{ option.label
             }}</el-radio-button>
         </el-radio-group>
         <el-color-picker size="small" v-else-if="form.type == 'color'" v-model="$value" @active-change="colorChange"
-            :show-alpha="form.alpha || false" @change="changeHook"></el-color-picker>
+            :show-alpha="form.alpha || false"></el-color-picker>
         <el-date-picker size="small" v-else-if="form.type == 'date'" v-model="$value" :type="form.subtype || 'datetime'"
-            :placeholder="form.placeholder" @change="changeHook"></el-date-picker>
+            :placeholder="form.placeholder"></el-date-picker>
+        <!-- 我只引入这一部分组件 有需要的可以自己扩展  -->
     </el-form-item>
 </template>
 
-<script lang='ts' setup>
-import { computed, ref, inject, onMounted } from "vue";
+<script lang="ts" setup>
+import { computed, ref, inject } from "vue";
 import { useConfigOption } from "../util/auto-options";
 import { useConfigVisiabled } from "../util/visiabled";
 import { useFiltersHtmlstrip } from "../util/htmlstrip";
@@ -65,7 +64,7 @@ interface DynamicFormItemProps {
     prop: string;
 }
 
-defineOptions({ name: 'DynamicFormItem' });
+defineOptions({ name: "DynamicFormItem" });
 const props = defineProps<DynamicFormItemProps>();
 const emits = defineEmits(["update:modelValue"]);
 const configs = inject("configs");
@@ -77,8 +76,8 @@ const $value = computed({
     },
     set(value) {
         emits("update:modelValue", value);
-    }
-})
+    },
+});
 
 // 生成options值
 const { options } = useConfigOption(props.config);
@@ -87,7 +86,6 @@ const { visiabled } = useConfigVisiabled(props.config, configValues);
 
 const form = computed(() => props.config.form);
 const loading = ref(false); // 先写着
-
 
 function colorChange(color: string) {
     $value.value = color;
@@ -99,23 +97,17 @@ const parseJson = (value: any) => {
         return JSON.stringify(JSON.parse(value));
     }
     return value;
-}
+};
 
 const $bvalue = form.value.blur ? ref(parseJson($value.value)) : $value;
 const blur = () => {
     if (form.value.blur) {
         $value.value = $bvalue.value;
     }
-}
-
-// 判断是否vip
-const changeHook = () => {
-
-}
-
+};
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .config-title {
     display: flex;
     align-items: center;
